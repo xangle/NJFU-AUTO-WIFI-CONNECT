2a68debf7eb005b1ddfd7e428181087baac8bf55
xangle <lvthislv@gmail.com>
2022-03-30 01:10:45 +0800

    update

--- NJFU-wifi-autoconnect.py
+++ NJFU-wifi-autoconnect.py
@@ -1,28 +1,18 @@
 import requests
 
-account = "w200350418@cmcc"
-password = '063515'
+account = "account"
+password = 'pwd'
 
 url = 'http://10.11.1.3/a70.htm'
 params = {
     "DDDDD": account,
     "upass": password,
     "R1": "0",
-    "R2": "",
     "R3": "0",
     "R6": "0",
     "para": "00",
     "para": "00",
-    "0MKKey": "123456",
-    "buttonClicked": "",
-    "redirect_url": "",
-    "err_flag": "",
-    "username": "",
-    "password": "",
-    "user": "",
-    "cmd": "",
-    "Login": "",
-    "v6ip": ""
+    "0MKKey": "123456"
 }
 
 res = requests.post(url = url, data = params)
